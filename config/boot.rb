06d6f1d213eb2b462c96ebcd064801f58284aa43
Branndon-Schneyder-Suarez-Jimenez <branndon255225@gmail.com>
2026-05-11 00:00:12 -0500

    fix: resolve git conflicts and ruby 4 yaml patch

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,13 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
+# PARCHE RUBY 4.0 PARA YAML
+require "yaml"
+if RUBY_VERSION >= "4.0" && YAML.respond_to?(:unsafe_load)
+  module YAML
+    class << self
+      alias_method :load, :unsafe_load
+    end
+  end
+end
